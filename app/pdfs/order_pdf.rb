20f017c095220ac8c9c0ad54df2fd29a123bf507
roberts-eihmanis <reihmanis@gmail.com>
2017-05-16 21:15:56 +0300

    Add fonts, fix pdf fonts

--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -1,6 +1,13 @@
 class OrderPdf < Prawn::Document
   def initialize(order)
     super(top_margin: 70)
+    font_families.update("OpenSans" => {
+      :normal => Rails.root.join("app/assets/fonts/OpenSans-Regular.ttf"),
+      :italic => Rails.root.join("app/assets/fonts/OpenSans-Regular.ttf"),
+      :bold => Rails.root.join("app/assets/fonts/OpenSans-Regular.ttf"),
+      :bold_italic => Rails.root.join("app/assets/fonts/OpenSans-Regular.ttf")
+    })
+    font "OpenSans"
     @order = order
     order_number
     line_items
@@ -10,17 +17,16 @@ class OrderPdf < Prawn::Document
   delegate :equipment_orders, to: :order
 
   def order_number
-    text "Pasutijums \##{@order.id}", size: 30, style: :bold
+    text "Pasūtijums \##{@order.id}".to_s, size: 30, style: :bold
   end
 
   def line_items
     move_down 20
-    table line_item_rows, :cell_style => { :font => "Helvetica", :size => 10, :text_color => "737373" } do
+    table line_item_rows, :cell_style => { :size => 10, :text_color => "737373" } do
       cells.padding = 12
       cells.borders = []
       row(0).borders = [:bottom]
       row(0).border_width = 1
-      row(0).font_style = :bold
       row(0).text_color = "000000"
       columns(0..7).borders = [:right]
       row(0).columns(0..7).borders = [:bottom, :right]
